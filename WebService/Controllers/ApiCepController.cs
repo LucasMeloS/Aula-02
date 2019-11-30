b1058c8ad8ed099519b143347cc18faf4a86f602
EduLFernandes <duhfsc4@gmail.com>
2019-11-30 16:16:11 -0300

    commitando projeto

---
+++ WebService/Controllers/ApiCepController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using WebService.Models;
+namespace WebService.Controllers
+{
+    public class ApiCepController : Controller
+    {
+        private readonly Context _context;
+        public ApiCepController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.ApiCep.ToList());
+        }
+
+        public IActionResult ApiCep()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ApiCep(string cep)
+        {
+            WebClient client = new WebClient();
+            string json = client.DownloadString("https://viacep.com.br/ws/" + cep + "/json/");
+            ApiCep cidade = JsonConvert.DeserializeObject<ApiCep>(json);
+
+            _context.Add(cidade);
+            _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+//Lucas de Melo da Silva
---
+++ WebService/Migrations/20191130174703_CriarBanco.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebService.Migrations
+{
+    public partial class CriarBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApiWebService",
+                columns: table => new
+                {
+                    CepId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cep = table.Column<string>(nullable: true),
+                    Logradouro = table.Column<string>(nullable: true),
+                    Complemento = table.Column<string>(nullable: true),
+                    Bairro = table.Column<string>(nullable: true),
+                    Localidade = table.Column<string>(nullable: true),
+                    Uf = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiWebService", x => x.CepId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiWebService");
+        }
+    }
+}
